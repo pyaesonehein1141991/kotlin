61e0abc6d16461ab4f34b8a6f35248e0d688fa82
pyos <pyos@google.com>
2020-03-04 15:16:48 +0100

    Add a test to check absence of NON_LOCAL_RETURN in inline suspend

--- js/js.tests/test/org/jetbrains/kotlin/js/test/ir/semantics/IrInlineSuspendTestsGenerated.java
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/ir/semantics/IrInlineSuspendTestsGenerated.java
@@ -108,6 +108,11 @@ public class IrInlineSuspendTestsGenerated extends AbstractIrInlineSuspendTests 
         runTestWithPackageReplacement("compiler/testData/codegen/boxInline/suspend/multipleSuspensionPoints.kt", "kotlin.coroutines");
     }
 
+    @TestMetadata("nonLocalReturn.kt")
+    public void testNonLocalReturn() throws Exception {
+        runTest("compiler/testData/codegen/boxInline/suspend/nonLocalReturn.kt");
+    }
+
     @TestMetadata("nonSuspendCrossinline.kt")
     public void testNonSuspendCrossinline_1_3() throws Exception {
         runTestWithPackageReplacement("compiler/testData/codegen/boxInline/suspend/nonSuspendCrossinline.kt", "kotlin.coroutines");
